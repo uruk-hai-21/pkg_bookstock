6d1b7762754b23a87c40bb4807cce616ea29077d
Uruk Hai 21 <francesco.zirconi@yahoo.it>
2025-06-04 12:57:08 +0200

    Update controller.php

--- admin/components/com_bookstock/controller.php
+++ admin/components/com_bookstock/controller.php
@@ -3,13 +3,13 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\MVC\Controller\BaseController;
 use Joomla\CMS\Router\Route;
+use Joomla\CMS\Factory;
 
 class BookstockController extends BaseController
 {
     public function display($cachable = false, $urlparams = [])
     {
-        // Carica la view "bookstock" se non specificata
-        $input = \Joomla\CMS\Factory::getApplication()->input;
+        $input = Factory::getApplication()->input;
         $input->set('view', $input->getCmd('view', 'bookstock'));
 
         return parent::display($cachable, $urlparams);
